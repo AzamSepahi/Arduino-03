6e0625afb5dac7ec93adf255db80b4400fa03707
Azam <n0ghli2002@gmail.com>
2024-10-20 15:11:12 +0330

    Update dancinglight1.ino

--- dancinglight1.ino
+++ dancinglight1.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   pinMode(A0, INPUT);
-  for (int i = 0; i <= 7; i++) {
+  for (int i = 0; i <= 3; i++) {
     pinMode(i, OUTPUT);
   }
 }
@@ -10,22 +10,18 @@ void setup() {
 void loop() {
   int i, a;
   int dancinglight[8][8] = {
-   {0,0,0,0,0,0,0,1},
-   {0,0,0,0,0,0,1,0},
-   {0,0,0,0,0,1,0,0},
-   {0,0,0,0,1,0,0,0},
-   {0,0,0,1,0,0,0,0},
-   {0,0,1,0,0,0,0,0},
-   {0,1,0,0,0,0,0,0},
-   {1,0,0,0,0,0,0,0}
+  {0,0,0,1},
+  {0,0,1,0},
+  {0,1,0,0},
+  {1,0,0,0}
   };
-  for (i = 0; i <= 7; i++)
+  for (i = 0; i <= 3; i++)
   {
     digitalWrite(i, 0);
   }
   if (key1 == 0) {
-    for (a = 0; a <= 7; a++) {
-      for (i = 0; i <= 7; i++) {
+    for (a = 0; a <= 3; a++) {
+      for (i = 0; i <= 3; i++) {
         digitalWrite(i, dancinglight[a][i]);
       }
       delay(200);
